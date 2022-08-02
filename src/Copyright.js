81cde1c02e51f99d547073b28b0957d59413af9d
Valeriya <kunichikval@gmail.com>
2022-08-03 01:45:52 +0600

    New

---
+++ src/Copyright.js
@@ -0,0 +1,10 @@
+function Copyright() {
+   
+    return (
+        <div>
+            <p className='copyright'>Copyright © 2022 Valerie Kunichik</p>
+        </div>
+    )
+}
+
+export default Copyright
